110d8f7f509bbf521017e098a09af1f88bcc2bc3
giacomotolari <tolarigiacomo@gmail.com>
2021-12-14 21:51:03 +0100

    add logout

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -12,62 +12,29 @@ const loginRouter = express.Router();
 // };
 
 loginRouter.post('/', async (req, res) => {
-  const username = req.body.userName;
-  let user = await UserModel.findOne({ userName: username });
+  const userName = req.body.userName;
+  let user = await UserModel.findOne({ userName: userName });
   if (!user) {
     user = await UserModel.findOne({ userName: 'anonymousUser' });
   }
-//   console.log(req.session.user);
+
   req.session.user = user;
   req.session.save();
   res.json(user);
 });
 
 loginRouter.get('/currentuser', async (req, res) => {
-  console.log(req.session);
   let user = req.session.user;
   if (!user) {
     user = await UserModel.findOne({ userName: 'anonymousUser' });
   }
-  res.json({user});
+  res.json({ user });
 });
 
 loginRouter.get('/logout', async (req, res) => {
   req.session.destroy();
   const user = await UserModel.findOne({ userName: 'anonymousUser' });
   res.json(user);
-});
-
-// loginRouter.post('/approveduser', async (req, res) => {
-//   const id = req.body.id;
-//   let user = req.session.user;
-//   if (!user) {
-//     console.log(1,user);
-//     res.sendStatus(403);
-//   } else {
-//     if (!userIsInGroup(user, 'admins')) {
-//       console.log(2,user);
-//       res.sendStatus(403);
-//     } else {
-//       const updateResult = await UserModel.findOneAndUpdate(
-//         { _id: new mongoose.Types.ObjectId(id) },
-//         { $set: { accessGroups: 'loggedInUsers,members' } },
-//         { new: true }
-//       );
-//       res.json({
-//         result: updateResult,
-//       });
-//     }
-//   }
-// });
-
-loginRouter.get('/notyetapprovedusers', async (req, res) => {
-  const users = await UserModel.find({
-    accessGroups: { $regex: 'notYetApprovedUsers', $options: 'i' },
-  });
-  res.json({
-    users,
-  });
 });
 
 export { loginRouter };
