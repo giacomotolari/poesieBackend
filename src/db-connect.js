9e43a240c150f252f68e61a3c258c40cd5ce4ee2
giacomotolari <tolarigiacomo@gmail.com>
2021-12-16 16:14:36 +0100

    change mondoDb string

--- src/db-connect.js
+++ src/db-connect.js
@@ -10,7 +10,7 @@ import mongoose from 'mongoose';
 const MONGO_URI = process.env.MONGO_URI;
 
 console.log({ MONGO_URI });
-
+console.log("");
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
