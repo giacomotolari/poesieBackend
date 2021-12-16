ee8ad9439a6191a78ea4744d2ac8ad85f0a939fb
giacomotolari <tolarigiacomo@gmail.com>
2021-12-16 16:53:04 +0100

    r

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.set('trust proxy', 1);
 app.use(
   cors({
     origin:
-      process.env.NODE_ENV !== "production"
+      process.env.NODE_ENV !== 'production'
         ? process.env.FRONTEND_URL
         : [process.env.FRONTEND_URL_HTTP, process.env.FRONTEND_URL_HTTPS],
     credentials: true,
